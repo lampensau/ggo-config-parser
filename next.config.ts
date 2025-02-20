3741aed01f2f3e281af2109e17f972b75f52f85a
Timo Toups <timo+lampensau@toups.eu>
2025-02-20 15:25:54 +0100

    fixed asset path and prevent incorrect parsing

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/ggo-config-parser' : '',
+  basePath: '/ggo-config-parser',
+  assetPrefix: '/ggo-config-parser/',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
