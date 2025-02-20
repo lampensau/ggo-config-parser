448ce9bc176ec5fe8efc5ed1f1faf00bad2fce0a
Timo Toups <timo+lampensau@toups.eu>
2025-02-20 16:53:46 +0100

    add favicon and improve description

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,22 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Green-GO Config Parser',
-  description: 'Parse and display Green-GO configuration files',
+  description: 'A simple application to parse Green-GO configuration files and export CSV files containing user and device information',
+  icons: {
+    icon: [
+      { url: '/ggo-config-parser/favicon.ico', sizes: 'any' },
+      { url: '/ggo-config-parser/favicon-32x32.png', type: 'image/png', sizes: '32x32' }
+    ]
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/ggo-config-parser/favicon.ico" sizes="any" />
+        <link rel="icon" href="/ggo-config-parser/favicon-32x32.png" type="image/png" sizes="32x32" />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
